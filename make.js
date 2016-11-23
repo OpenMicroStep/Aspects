022179e3590f0faccd8fa2e48fb765eece0cf24a
Eric Baradat <e-baradat@logitud.fr>
2016-11-23 14:39:31 +0100

    make

--- make.js
+++ make.js
@@ -1,15 +1,27 @@
 module.exports =  {
   is: "project",
   name: "Aspects",
-  "ts=": { is: 'environment', type: "javascript", packager: "npm", compiler: "typescript" },
+  "ts=": { is: 'environment', type: "javascript", packager: "npm", compiler: "typescript",
+      tsConfig: [{
+        "module": "commonjs",
+        "target": "es6",
+        "declaration": true,
+        "sourceMap": true,
+        "experimentalDecorators": true,
+        "strictNullChecks": true,
+        "noImplicitThis": true,
+        "noImplicitReturns": true,
+        "lib": ["es6"],
+        "types": ["node"]
+    }],},
   "Files=": { is: 'group', elements: [
-      { is: 'group', name: 'typescript', elements: [
+      { is: 'group', name: 'typescript', path: 'typescript/src', elements: [
         { is: 'file', name: '*.ts' }
       ]}
   ]},
   "Typescript aspects=": {
     is: 'target',
     environments: ["=ts"],
-    files: ["=Files:ts"],
+    files: ["=Files:typescript"],
   }
 };
