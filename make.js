e43a8c42c8a6a6be2219c5d520cb73cef1d54237
Vincent Rouillé <v-rouille@logitud.fr>
2017-02-16 23:09:25 +0100

    bump version

--- make.js
+++ make.js
@@ -3,7 +3,7 @@ module.exports =  {
   name: "Aspects",
   "ts base=": { is: 'component', type: "javascript", compiler: "aspects",
       npmPackage: [{
-        "version": "0.2.6",
+        "version": "0.2.7",
       }],
       tsConfig: [{
         "module": "commonjs",
