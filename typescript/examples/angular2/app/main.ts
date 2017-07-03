2a372741709f6f8ec0149765936f25bdb8c7b8a4
Vincent Rouillé <v-rouille@logitud.fr>
2017-07-03 10:58:47 +0200

    example.angular2: fix build

--- typescript/examples/angular2/app/main.ts
+++ typescript/examples/angular2/app/main.ts
@@ -10,12 +10,12 @@ export const PersonClient = Person.installAspect(controlCenter, "client");
 export const dataSource = new (DataSource.installAspect(controlCenter, "client"))();
 export const app = new DemoAppClient();
 const xhr = new XHRTransport();
-controlCenter._aspects.forEach(cstor => {
+for (let cstor of controlCenter.installedAspectConstructors()) {
     cstor.aspect.farMethods.forEach(method => {
         if (method.transport === Aspect.farTransportStub)
             method.transport = xhr;
     });
-});
+}
 platformBrowserDynamic().bootstrapModule(AppModule);
 
 app.manager().setId('__root');
